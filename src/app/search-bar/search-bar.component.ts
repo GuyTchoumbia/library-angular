ad3f38030ffa1304ec30dd2da3b766f643a1742d
GuyTchoumbia <guy.tchoumbia@gmail.com>
2019-06-13 09:13:03 +0200

    sqdsqdq

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  libelle = new FormControl('');
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
